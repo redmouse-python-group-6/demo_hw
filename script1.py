f0ec9b7c41c2df006359c6edf168bad401879002
pehas <pehas.90@gmail.com>
2017-04-18 19:18:35 +0600

    Create hw1_2

---
+++ script1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+ 
+print u"Общество в начале XXI века"
+age=int(raw_input("Введите ваш возраст "))
+if 0<=age<=7:
+    print u"Вам в детский сад"
+elif age<=18:
+    print u"Вам в школу"
+elif age<=25:
+    print u"Вам в профессиональное учебное заведение"
+elif age<=60:
+    print u"Вам на работу"
+elif age<=120:
+    print u"Вам предоставляется выбор"
+else:
+    error = u"Ошибка! Это программа для людей!\n"
+    print error*5
